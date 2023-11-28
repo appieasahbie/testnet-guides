018246765c23356a013854ab3304fca380668255
APPIE | AkNodes <108979536+appieasahbie@users.noreply.github.com>
2023-11-29 00:20:43 +0100

    Update common.sh

--- utils1/common.sh
+++ utils1/common.sh
@@ -1,28 +1,37 @@
-GREEN="\e[1m\e[1;32m" # green color
-RED="\e[1m\e[1;31m" # red color
-BLUE='\033[0;34m'   # blue color
-NC="\e[0m"          # no color
+#!/bin/bash
 
+# Color Definitions
+GREEN="\e[1m\e[1;32m" # Green color
+RED="\e[1m\e[1;31m"   # Red color
+BLUE='\033[0;34m'     # Blue color
+NC="\e[0m"            # No color
+
+# Function to print the logo
 function printLogo {
-  bash <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/logo.sh)
+  bash <(curl -s https://raw.githubusercontent.com/appieasahbie/testnet-guides/main/utils/logo.sh)
 }
 
+# Function to print a line separator
 function printLine {
   echo "------------------------------------------------------------------------------------"
 }
 
+# Function to print in green color
 function printGreen {
   echo -e "${GREEN}${1}${NC}"
 }
 
+# Function to print in red color
 function printRed {
   echo -e "${RED}${1}${NC}"
 }
 
+# Function to print in blue color
 function printBlue {
   echo -e "${BLUE}${1}${NC}"
 }
 
+# Function to add a directory to the PATH
 function addToPath {
   source $HOME/.bash_profile
   PATH_EXIST=$(grep "${1}" $HOME/.bash_profile)
